44ab1f4f9c6cad7f2a7bb6f3e6c711393ecb6a19
SimzMapisa <simbarashe795@gmail.com>
2024-08-18 08:01:09 +0000

    fix code indentation

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
-# Apache 500 error by fixing typo in wordpress settings file
+# Apache 500 error by fixing typo in wordpress settings config file
+# Fixes typo phpp to php
 
-exec { 'fix typo':
-	onlyif  => 'test -e /var/www/html/wp-settings.php',
-	command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-	path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'fix typo in a wordpress website':
+    onlyif  => 'test -e /var/www/html/wp-settings.php',
+    command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
